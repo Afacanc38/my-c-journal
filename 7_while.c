4d83903b11fb4482c38798920bcd83e4688c689a
Alperen İsa Nalbant <66299502+Afacanc38@users.noreply.github.com>
2022-07-06 12:23:45 +0300

    important description

--- 7_while.c
+++ 7_while.c
@@ -13,6 +13,48 @@ int main() {
   do {
     printf("Bu kod %d kez tekrarlandı.\n", i);
     ++i;
+    /*
+     * Şimdi yukarıdaki satırı gördüğünüzde
+     * aklınıza şöyle bir soru gelmiş olabilir.
+     * 14 | ...
+     * 15 | ++i;
+     *    |
+     * "Bunun yerine i++ kullanamaz mıydık?"
+     *
+     * Evet, kullanabilirdik. Kodumuz sıkıntısız
+     * bir şekilde de çalışırdı.
+     *
+     * Ancak şunu bilmekte fayda var. i++ ile
+     * ++i farklı fonksiyonlardır.
+     *
+     * Aralarındaki farkı şöyle izah edeyim;
+     * Şu iki kod bloğunu inceleyin;
+     * 
+     * Örnek 1:
+     * 1 | int a = 1;
+     * 2 | int b = ++a;
+     * 3 | printf("%d", a);
+     * 4 | printf("%d", b);
+     * -- çıktı ----------
+     * 2
+     * 2
+     *
+     * Örnek 2:
+     * 1 | int a = 1;
+     * 2 | int b = a++;
+     * 3 | printf("%d", a);
+     * 4 | printf("%d", b);
+     * -- çıktı ----------
+     * 1
+     * 2
+     *
+     * Yani değişkenin başına ++ koymak
+     * değişkenin değerini değiştirirken
+     * sonuna koymak değişkeni değiştirmez.
+     *
+     * Bu yüzden döngülerde bu şekilde
+     * kullanmak doğrudur.
+     */
   }
   while (i < 5); // i 5'den bir küçük sayıya ulaştığında durdur
 
